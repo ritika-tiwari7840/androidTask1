91f273eed8ab191f66760ee95bf8dee6fdf1e3ce
Ritika <ritikatiwari784@gmail.com>
2024-10-19 22:21:09 +0530

    linked activities and add activities in AndroidManifest.xml file

--- app/src/main/java/com/ritika/task/MainActivity.kt
+++ app/src/main/java/com/ritika/task/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ritika.task
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main)
+
+        // Set up the OnApplyWindowInsetsListener
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+
+        val loginBtn: Button = findViewById(R.id.welcome_login)
+        loginBtn.setOnClickListener {
+            // Intent to navigate to SecondActivity
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            }
+
+
+        val regBtn: Button = findViewById(R.id.welcome_reg)
+        regBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+
         }
     }
 }
